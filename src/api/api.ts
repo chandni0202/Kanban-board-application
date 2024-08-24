899ceec649c714e6cc297167761ea1cb9a61c75e
Chandni Tushar Sharma <chandnimishra02@gmail.com>
2024-08-24 12:09:42 +0530

    remove url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,5 @@
 import mockData from  './mockData.json'
 // const API_URL = 'UR URL'; // Please check mockData.json for json structure;
-
 const fetchData = async () => {
   // try {
   //   const response = await fetch(API_URL);
