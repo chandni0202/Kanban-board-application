26a846677906e1c8e63305439db6224e4b24b1dd
Chandni Tushar Sharma <chandnimishra02@gmail.com>
2024-08-24 12:09:21 +0530

    generated random profile images

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,14 @@
-// import mockData from  './mockData.json'
-// Please check mockData.json for json structure;
-const API_URL = 'https://api.quicksell.co/v1/internal/frontend-assignment'; 
+import mockData from  './mockData.json'
+// const API_URL = 'UR URL'; // Please check mockData.json for json structure;
 
 const fetchData = async () => {
-  try {
-    const response = await fetch(API_URL);
-    return await response.json();
-  } catch (error) {
-    console.error('Failed to fetch data:', error);
-  }
-  // return mockData;
+  // try {
+  //   const response = await fetch(API_URL);
+  //   return await response.json();
+  // } catch (error) {
+  //   console.error('Failed to fetch data:', error);
+  // }
+  return mockData;
 };
 
 export default fetchData;
